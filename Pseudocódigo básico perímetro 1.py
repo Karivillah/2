ecdae364042908c7daa90e3767ae4367ebf9aaf2
Karivillah <111943870+Karivillah@users.noreply.github.com>
2022-08-25 12:57:13 -0500

    Add files via upload

---
+++ Pseudocódigo básico perímetro 1.py
@@ -0,0 +1,11 @@
+# Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+# Es posible que el codigo generado no sea completamente correcto. Si encuentra
+# errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
+
+if __name__ == '__main__':
+	print("Introduce la medida de un lado del triángulo equilátero")
+	lado = float(input())
+	perimetro = lado*3
+	print("El resultado del perímetro es  ",perimetro)
+
